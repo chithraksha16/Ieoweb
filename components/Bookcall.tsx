1f212f322b6f6b84a9ad096cb256d7859f86b002
chithraksha16 <chithrakshakharvi@gmail.com>
2026-08-28 19:21:33 +0530

    updated bookcall and added cal.com interface

--- components/Bookcall.tsx
+++ components/Bookcall.tsx
@@ -1,11 +1,37 @@
-import React from 'react'
+"use client";
 
-const Bookcall = () => {
+import Cal, { getCalApi } from "@calcom/embed-react";
+import { useEffect } from "react";
+
+export default function Bookcall() {
+  useEffect(() => {
+    (async function () {
+      const cal = await getCalApi({
+        namespace: "30min",
+      });
+
+      cal("ui", {
+        hideEventTypeDetails: false,
+        layout: "month_view",
+      });
+    })();
+  }, []);
+
   return (
-    <div>
-      meeting
+    <div className="w-full">
+      <Cal
+        namespace="30min"
+        calLink="ieo-web/30min"
+        style={{
+          width: "100%",
+          height: "700px",
+          overflow: "scroll",
+        }}
+        config={{
+          layout: "month_view",
+          useSlotsViewOnSmallScreen: "true",
+        }}
+      />
     </div>
-  )
+  );
 }
-
-export default Bookcall
